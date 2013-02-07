321ba1fe86f8ec23648e0af1e97b8d199a3da50e
Joel <joebukkit@gmail.com>
2013-02-07 06:37:38 -0600

    Fixed spacing
    Fixed the spacing to fit inside of the user's web browser.

--- MainClass.java
+++ MainClass.java
@@ -2,12 +2,17 @@
  * Simply put, the point of this project is to explain Java to brand new coders in English on Microsoft Windows.
  * 
  * You will need a few things before being able to write code in Java:
- * 1: The 32 bit version of Java. Java can be downloaded at http://www.java.com/en/download/manual.jsp After you download Java, install it. If you don't know how to install Java, you should probably quit now.
- * 2: The 32 bit version of the Eclipse IDE for Java EE Developers. Eclipse downloaded at http://www.eclipse.org/downloads/ After you have downloaded Eclipse, extract it to wherever you want then run Eclipse from inside the Eclipse folder.
+ * 1: The 32 bit version of Java. Java can be downloaded at http://www.java.com/en/download/manual.jsp After you
+ * download Java, install it. If you don't know how to install Java, you should probably quit now.
+ * 2: The 32 bit version of the Eclipse IDE for Java EE Developers. Eclipse downloaded at
+ * http://www.eclipse.org/downloads/ After you have downloaded Eclipse, extract it to wherever you want then run Eclipse
+ * from inside the Eclipse folder.
  * After you get Eclipse all set up, click File -> New -> Project
- * Make a new Java project named ConsoleProject. After that you will find an icon right below file, click it and create a new class with the name MainClass.
- * Look in the top right corner and make sure that Java EE is selected and not Java. Then in the left corner expand ConsoleProject (If it isn't already), then src, then (default package), and double click MainClass.java
- * If there is currently no code inside MainClass.java, copy and paste the code below into the text field:
+ * Make a new Java project named ConsoleProject. After that you will find an icon right below file, click it and create
+ * a new class with the name MainClass. Look in the top right corner and make sure that Java EE is selected and not Java.
+ * Then in the left corner expand ConsoleProject (If it isn't already), then src, then (default package), and double
+ * click MainClass.java If there is currently no code inside MainClass.java, copy and paste the code below into the text
+ * field:
 
 public class MainClass
 {
@@ -17,7 +22,9 @@ public class MainClass
 	}
 }
 
- * READ: I'm pretty tired, so I'm going to go to bed. If anybody cares to commit in more information, please do it. Do not import things, or it might confuse the user. I plan on importing things inside of external class files later, such as the Scanner. Always make sure there is a new line at the end of the file.
+ * READ: I'm pretty tired, so I'm going to go to bed. If anybody cares to commit in more information, please do it. Do
+ * not import things, or it might confuse the user. I plan on importing things inside of external class files later,
+ * such as the Scanner. Always make sure there is a new line at the end of the file for GitHub.
  */
 public class MainClass
 {
