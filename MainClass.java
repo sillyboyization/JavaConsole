76b8de19681d5d3832ecde26e13f9bb97424aadb
Joel <joebukkit@gmail.com>
2013-02-07 06:43:03 -0600

    Oops + More spacing

--- MainClass.java
+++ MainClass.java
@@ -2,12 +2,16 @@
  * Simply put, the point of this project is to explain Java to brand new coders in English on Microsoft Windows.
  * 
  * You will need a few things before being able to write code in Java:
+ *
  * 1: The 32 bit version of Java. Java can be downloaded at http://www.java.com/en/download/manual.jsp After you
  * download Java, install it. If you don't know how to install Java, you should probably quit now.
- * 2: The 32 bit version of the Eclipse IDE for Java EE Developers. Eclipse downloaded at
+ *
+ * 2: The 32 bit version of the Eclipse IDE for Java EE Developers. Eclipse can be downloaded at
  * http://www.eclipse.org/downloads/ After you have downloaded Eclipse, extract it to wherever you want then run Eclipse
  * from inside the Eclipse folder.
+ *
  * After you get Eclipse all set up, click File -> New -> Project
+ *
  * Make a new Java project named ConsoleProject. After that you will find an icon right below file, click it and create
  * a new class with the name MainClass. Look in the top right corner and make sure that Java EE is selected and not Java.
  * Then in the left corner expand ConsoleProject (If it isn't already), then src, then (default package), and double
